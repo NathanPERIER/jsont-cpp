1197c3a96a26a46bd6aa271e46ff2146fb9fdf81
Nathan PERIER <periernathan2000@gmail.com>
2023-10-20 21:12:52 +0900

    Added automatic deserializer for optionals

--- lib/jsont/deserializers/base.hh
+++ lib/jsont/deserializers/base.hh
@@ -1,7 +1,10 @@
 
 #pragma once
 
+#include <optional>
+
 #include <jsont/value.hh>
+#include <jsont/error.hh>
 
 
 namespace jsont {
@@ -16,6 +19,18 @@ template <typename T>
 constexpr bool is_json_deserializable() {
 	return json_deserialize_helper<T>::can_deserialize;
 }
+
+
+template <typename T>
+struct json_deserialize_helper<std::optional<T>> {
+	static constexpr bool can_deserialize = is_json_deserializable<T>();
+	static std::optional<T> deserialize(std::shared_ptr<json_node_value> json) {
+		if(json->data.is_null()) {
+			return std::nullopt;
+		}
+		return std::optional<T>(json_deserialize_helper<T>::deserialize(json));
+	}
+};
 
 } // namespace jsont
 
