08614b27c54bd21788e5906b08548550683a00fb
Nathan PERIER <periernathan2000@gmail.com>
2023-10-20 17:59:51 +0900

    Added base error for reading JSON

---
+++ lib/jsont/error.hh
@@ -0,0 +1,23 @@
+
+#pragma once
+
+#include <string>
+#include <exception>
+
+namespace jsont {
+
+class json_exception: public std::exception {
+
+public:
+	json_exception(const std::string& message): _message(message) { }
+
+	const char* what() const noexcept {
+		return _message.c_str();
+	}
+
+private:
+	const std::string _message;
+
+};
+
+} // namespace jsont
