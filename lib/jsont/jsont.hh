c3dfe3f2aeb60fcee07610f15c2cb4c875626cb0
Nathan PERIER <periernathan2000@gmail.com>
2023-11-14 23:00:54 +0100

    Temp code storage

---
+++ lib/jsont/jsont.hh
@@ -0,0 +1,10 @@
+
+#pragma once
+
+// TODO
+//  - Add fmt dependency
+//  - Pretty print for exceptions
+//  - Custom deserializers for useful stuff
+//  - Maybe a bit more try/catch
+//  - tests
+
--- tests/main.cc
+++ tests/main.cc
@@ -1,7 +1,15 @@
 
+#include <iostream>
+
+#include <jsont/node.hh>
 
 int main(int argc, char** argv) {
-	
+	// std::string s = jsont::json_node("{\"valu\": \"lol\"}")["value"].as<std::string>();
+	// std::string s = jsont::json_node("{\"value\": \"lol\"}").get_opt<std::string>("valu").value_or("null");
+	std::string s = jsont::json_node("[\"value\", \"lol\"]").get<std::string>(1);
+
+	std::cout << s << std::endl;
+
 	return 0;
 }
 
